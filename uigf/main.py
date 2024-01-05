17e61dcfa7ee1550793d6e6ce05b17931be76105
Erlone_alpha <zhongerlong@gmail.com>
2024-01-06 01:51:33 +0800

    增加了正确的json数据导入，增加了预处理的数据

--- uigf/main.py
+++ uigf/main.py
@@ -1,9 +1,13 @@
 import os
+from os.path import join as join_
+from os.path import dirname as dirname_
+from os.path import abspath as abspath_
+from os.path import basename as basename_
 import json
 from typing import List, Dict, Set
 import pandas as pd
 
-samplejson: dict = json.load(open('./samplejson.json', encoding='utf-8'))
+# samplejson: dict = json.load(open('./samplejson.json', encoding='utf-8'))
 
 class TPoolType():
     def __init__(self) -> None:
@@ -90,13 +94,101 @@ class UigfJsonType():
             self.lst_col.append(key_)
             self.lst_data.append(val_)
             self.time_lst.append(self.list.time)
+# block_bool = False
+# if block_bool:
+#     for key, val in samplejson.items():
+#         if key == "properties":
+#             info = val['info']["properties"]
+#             list = val['list']["items"]
+#             uigf = UigfJsonType()
+#             uigf.parse(info, list)
 
-for key, val in samplejson.items():
-    if key == "properties":
-        info = val['info']["properties"]
-        list = val['list']["items"]
-        uigf = UigfJsonType()
-        uigf.parse(info, list)
+    # # 数据统计
+    # df = pd.Series(column= uigf.lst_col, row= uigf.lst_data, index=uigf.time_lst)
 
-# 数据统计
-df = pd.Series(column= uigf.lst_col, row= uigf.lst_data, index=uigf.time_lst)
+path_j = r"F:\Documents\python_script\脚本\genshin_ufgi-fix\uigf_data_fix\uigf\uigf_json\UIGF_1.json"
+path_b = dirname_(abspath_(__file__))
+with open(join_(path_b, '.\\uigf_json\\UIGF_1.json'), 'r', encoding='utf-8') as f:
+    uigf_json: dict = json.load(f)
+
+gacha_info = uigf_json['info']
+gacha_list: List[dict] = uigf_json['list']
+
+df_gacha = pd.DataFrame(gacha_list)
+df_gacha.info()
+# 显示df前5行
+print(df_gacha.head() )
+
+# 新建一个dataframe 根据列 'name' 筛选出唯一的数据
+df_gacha_unique = df_gacha.drop_duplicates(subset='name')
+
+# 然后根据 item_type进行分组
+df_gacha_unique_group = df_gacha_unique.groupby('item_type')
+# 最后将分组的结果存储为列表[字典,...]
+result_list = []
+for name, group in df_gacha_unique_group:
+    result_list.append(group.to_dict('records'))
+if result_list[0][0]['item_type'] == '武器':
+    weapon_list = result_list[0]
+    char_list = result_list[-1]
+
+df_gacha_char = pd.DataFrame(char_list)
+df_gacha_weapon = pd.DataFrame(weapon_list)
+
+df_gacha_char_group = df_gacha_char.groupby('uigf_gacha_type')
+df_gacha_weapon = df_gacha_weapon.groupby('uigf_gacha_type')
+
+time_start, time_end = '2021-01-02 12:00:00', '2022-06-11 22:00:00'
+limited_char_time_interval = (time_start, time_end)
+limited_cur_char_dic = {
+    '琴':         [79, ['2020-12-23 18:00:00', '2021-01-12 00:00:00']], # 第一个五星
+    '魈':         [79, ['2021-02-03 18:00:00', '2021-02-23 00:00:00']], # 当时我居然为魈 -648, 好像还充了，忘了多少了
+    '莫娜':       [80, ['2021-3-16 18:00:00']],                         # 有印象记得是什么时候出的时间区间为准确的一天 (这个我甚至记得是在什么时候出的, 没错就是在卡池刚开没多久)
+    '胡桃':       [71, ['2021-3-16 18:00:00']],                         # 当时歪莫娜之后，立马充了648把胡桃抽出来了 QAQ
+    '七七':       [33, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 好像是一天的中午刚强化完胡桃的圣遗物抽出来的, 还兴奋了挺久
+    '钟离':       [76, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 具体时间记不清楚了，但肯定不是卡池出的那一天
+    '七七':       [50, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 这个和优菈是几乎同时出的, 当时我直接跳起来了...
+    '优菈':       [9,  ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 第一次10连双金
+    '迪卢克':     [76, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 痛苦的经历，当时家里电脑暂时没法用了就去网吧抽可莉。 结果...
+    '可莉':       [76, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 本人很喜欢可莉, 所以... -648...
+    '枫原万叶':   [11, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 卡池刚开的时候应该是
+    '琴':         [39, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 应该和上个卡池隔了半个版本
+    '神里绫华':   [20, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 好像是卡池第二天
+    '宵宫':       [39, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 应该是卡池刚出的时候
+    '珊瑚宫心海': [75, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 卡池前中期？
+    '胡桃':       [77, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 卡池中后期？
+    '阿贝多':     [36, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 应该是卡池刚开的时候
+    '申鹤':       [77, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 卡池前中期
+    '甘雨':       [53, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 卡池第一天or刚开卡池
+    '八重神子':   [74, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 卡池第一天or第二天
+    '雷电将军':   [72, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 卡池前中期
+    '刻晴':       [39, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 卡池前期  终结了9连没歪
+    '夜兰':       [80, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']],  # 应该是卡池末期
+    '':           [17, ['2021-02-23 18:00:00', '2021-3-16 00:00:00']]   # 这个是未出货抽数 #TODO 这里的数据和之后的数据合并可能会出现保底大于180的情况, 得特殊处理
+}                                                                       # 还有有半年的数据是丢失的(悲) 但是可以通过已经获取的角色猜测, 歪的是谁就不清楚了，但我只知道琴11命...
+
+time_start_, time_end_ = '2020-11-28 12:00:00', '2022-05-19 22:00:00'
+limited_weapon_time_interval = (time_start_, time_end_)
+weapon_cur_dic = {
+    '和璞鸢':       [35, '2021'],
+    '狼的末路':     [65, '2021'],
+    '天空之刃':     [68, '2021'],
+    '终末嗟叹之诗': [43, '2021'],
+    '护摩之杖':     [65, '2021'],
+    '薙草之稻光':   [53, '2021'],
+    '天空之傲':     [71, '2021'],
+    '':             [2, '2021']
+}
+
+time_start__, time_end__ = '2020-11-28 12:00:00', '2022-06-11 22:00:00'
+limited_permanent_time_interval = (time_start__, time_end__)
+permanent_cur_char_dic = {
+    '琴':       [79, '2021'],
+    '天空之卷': [83, '2021'],
+    '七七':     [79, '2021'],
+    '风鹰剑':   [16, '2021'],
+    '天空之傲': [73, '2021'],
+    '':         [19, '2021']
+}
+
+print()
