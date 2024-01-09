eff0012b5417aac1898481e7840607e53a14666a
Erlone_alpha <zhongerlong@gmail.com>
2024-01-09 15:18:35 +0800

    updata

--- uigf/main.py
+++ uigf/main.py
@@ -290,41 +290,82 @@ def nomalize_data(
     
 def load_tmp_data(path):
     with open(path, 'r', encoding='utf-8')as f:
-        lst = []
+        
         lines = f.readlines()
-        i = 0
-        while i < len(lines):
-            j = 0
-            line_cont_1 = lines[i].split('\t')
-            line_cont_2 = lines[i+1].split('\t')
-            line_cont_3 = lines[i+2].split('\t')
-            if line_cont_1[4] == '祈愿' or line_cont_1[4] == '限定祈愿':
-                pass
-            else:
-                i+=3
-                continue
-            if line_cont_1[3] == '5星.png':
-                rank_type = 5
-            elif line_cont_1[3] == '4星.png':
-                rank_type = 4
-            elif line_cont_1[3] == '3星.png':
-                rank_type = 3
-                print('error')
-            dic = {
-                'name': line_cont_1[1],
-                'type': line_cont_1[2],
-                'rank_type': rank_type
-            }
-            lst.append(dic)
-            i += 3
+        # i = 0
+        # while i < len(lines):
+        #     j = 0
+        #     line_cont_1 = lines[i].split('\t')
+        #     line_cont_2 = lines[i+1].split('\t')
+        #     line_cont_3 = lines[i+2].split('\t')
+        #     if line_cont_1[4] == '祈愿' or line_cont_1[4] == '限定祈愿':
+        #         pass
+        #     else:
+        #         i+=3
+        #         continue
+        #     if line_cont_1[3] == '5星.png':
+        #         rank_type = 5
+        #     elif line_cont_1[3] == '4星.png':
+        #         rank_type = 4
+        #     elif line_cont_1[3] == '3星.png':
+        #         rank_type = 3
+        #         print('error')
+        #     dic = {
+        #         'name': line_cont_1[1],
+        #         'type': line_cont_1[2],
+        #         'rank_type': rank_type
+        #     }
+        #     lst.append(dic)
+        #     i += 3
         contents = []
+        first = True
         for i, line in enumerate(lines):
             if line.startswith('['):
                 section_name = line.strip('[]\n')
                 contents.append({
                     'section': section_name,
-                    'index': i+1
+                    'indexL': i+1,
+                    'indexR': None
                 })
+                if not first:
+                    if contents[len(contents)-2]['indexL'] >= i-1:
+                        contents[len(contents)-2]['indexR'] = contents[len(contents)-2]['indexL']
+                    else:
+                        contents[len(contents)-2]['indexR'] = i-1
+                    
+                first = False
+            if i == len(lines)-1:
+                contents[len(contents)-1]['indexR'] = i
+        content_lst = []
+        for content in contents:
+            if not content['indexL'] == content['indexR']:
+                content_lst.append({'name': content['section'],
+                                    'context':lines[content['indexL']:content['indexR']], 
+                                    })
+        lst_vall = []
+        for content in content_lst:
+            lst = []
+            cur_contents = [line.split('\t') for line in content['context'] if line.split('\t')[0].endswith('.png')]
+            for cur_content in cur_contents:
+                if cur_content[4] == '祈愿' or cur_content[4] == '限定祈愿':
+                    pass
+                else:
+                    continue
+                if cur_content[3] == '5星.png':
+                    rank_type = 5
+                elif cur_content[3] == '4星.png':
+                    rank_type = 4
+                elif cur_content[3] == '3星.png':
+                    rank_type = 3
+                dic = {
+                    'name': cur_content[1],
+                    'type': cur_content[2],
+                    'rank_type': 5
+                }
+                lst.append(dic)
+            lst_vall.append({
+                "version": content['name'],
+                "context": lst})
     return lst
         
             
